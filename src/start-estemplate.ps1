f061324d4598c6b5ac0403e6d22a4a4cc5316243
Darrell Lowrance <djl2@users.noreply.github.com>
2018-06-15 16:56:54 -0500

    Add new test script and edit Master Node scripts
    Removed Availability Sets sections
    Added section for Zones

---
+++ src/start-estemplate.ps1
@@ -0,0 +1,33 @@
+<# Start-estemplate.ps
+
+#>
+
+# Enable all debug output
+$DebugPreference = "Continue"
+
+$clusterParameters = @{
+    "artifactsBaseUrl"="https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/dev/src"
+    "esVersion" = "6.2.1"
+    "esClusterName" = "elasticsearch"
+    "loadBalancerType" = "internal"
+    "vmDataDiskCount" = 1
+    "adminUsername" = "russ"
+    "adminPassword" = "Password1234"
+    "securityAdminPassword" = "Password123"
+    "securityReadPassword" = "Password123"
+    "securityKibanaPassword" = "Password123"
+    "securityLogstashPassword" = "Password123"
+}
+
+
+# Capture all debug info in $output
+# Note that 5>&1 is a PS redirector operator. Required for capturing the debug output.
+$output = Test-AzureRmResourceGroupDeployment `
+    -ResourceGroupName "estemplate-poc-rg" `
+    -TemplateUri "https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/dev/src/mainTemplate.json" `
+    -TemplateParameterObject $clusterParameters `
+    -Verbose `
+    5>&1
+
+# Run the output for capture debug info
+$output | out-file .\start-estemplate.log
