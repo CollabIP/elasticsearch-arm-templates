7e4c525e6fb5ea6d632056665531008da1cc777f
Darrell Lowrance <djl2@users.noreply.github.com>
2018-06-26 15:10:36 -0500

    Create new script to deploy to Azure
    Based on test script.  Does actual deploy

---
+++ src/new-estemplate.ps1
@@ -0,0 +1,39 @@
+<#new-estemplate.ps
+
+Deploys single EZ node.
+
+#>
+
+# Enable all debug output
+# $DebugPreference = "Continue"
+
+$clusterParameters = @{
+    "artifactsBaseUrl"="https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/feature-deploy-single-node-type/src"
+    "esVersion" = "6.2.1"
+    "esClusterName" = "elasticsearch"
+    "loadBalancerType" = "internal"
+    "nodeType" = "master"
+    "vmId" = "1"
+    "zoneId" = "1"
+    "vmDataDiskCount" = 1
+    "vmHostNamePrefix" = "ctesd"
+    "adminUsername" = "russ"
+    "adminPassword" = "Password1234"
+    "securityAdminPassword" = "Password123"
+    "securityReadPassword" = "Password123"
+    "securityKibanaPassword" = "Password123"
+    "securityLogstashPassword" = "Password123"
+}
+
+
+# Capture all debug info in $output
+# Note that 5>&1 is a PS redirector operator. Required for capturing the debug output.
+$output = New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName "estemplate-poc-rg" `
+    -TemplateUri "https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/feature-deploy-single-node-type/src/mainTemplate.json" `
+    -TemplateParameterObject $clusterParameters `
+    -Verbose # `
+    5>&1
+
+# Run the output for capture debug info
+# $output | out-file .\start-estemplate.log
