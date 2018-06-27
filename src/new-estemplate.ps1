f180752ede3085f176bdfc9a3ee6593a97f18195
Darrell Lowrance <djl2@users.noreply.github.com>
2018-06-27 11:02:48 -0500

    Changed default cluster name to -dev

--- src/new-estemplate.ps1
+++ src/new-estemplate.ps1
@@ -1,6 +1,6 @@
 <#new-estemplate.ps
 
-Deploys single EZ node.
+Deploys single ES node.
 
 #>
 
@@ -10,7 +10,7 @@ Deploys single EZ node.
 $clusterParameters = @{
     "artifactsBaseUrl"="https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/feature-deploy-single-node-type/src"
     "esVersion" = "6.2.1"
-    "esClusterName" = "elasticsearch"
+    "esClusterName" = "elastic-dev"
     "loadBalancerType" = "internal"
     "nodeType" = "master"
     "vmId" = "1"
@@ -32,8 +32,7 @@ $output = New-AzureRmResourceGroupDeployment `
     -ResourceGroupName "estemplate-poc-rg" `
     -TemplateUri "https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/feature-deploy-single-node-type/src/mainTemplate.json" `
     -TemplateParameterObject $clusterParameters `
-    -Verbose # `
-    5>&1
+    -DeploymentDebugLogLevel All
 
 # Run the output for capture debug info
 # $output | out-file .\start-estemplate.log
