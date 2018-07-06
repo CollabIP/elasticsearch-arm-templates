00485c42476f494f95ab8e8928383ac895ab157e
Darrell Lowrance <djl2@users.noreply.github.com>
2018-07-06 15:57:04 -0500

    enable wait_for_started
    Will test using the matching bootstrap pw

--- src/scripts/elasticsearch-ubuntu-install.sh
+++ src/scripts/elasticsearch-ubuntu-install.sh
@@ -896,10 +896,10 @@ start_monit
 
 # 6.29.18 DISABLED for now due to timeout failure during VM creation script.  NEEDS RESEARCH
 # patch roles and users through the REST API which is a tad trickier
-# if [[ ${INSTALL_XPACK} -ne 0 ]]; then
-#  wait_for_started
-#  apply_security_settings
-#fi
+if [[ ${INSTALL_XPACK} -ne 0 ]]; then
+wait_for_started
+apply_security_settings
+fi
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 PRETTY=$(printf '%dh:%dm:%ds\n' $(($ELAPSED_TIME/3600)) $(($ELAPSED_TIME%3600/60)) $(($ELAPSED_TIME%60)))
