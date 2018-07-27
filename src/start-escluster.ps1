38eabe4cfbc5988f5143493fd4c04cef13b9c051
Darrell Lowrance <djl2@users.noreply.github.com>
2018-07-27 09:20:45 -0500

    Add waiting dots in message

--- src/start-escluster.ps1
+++ src/start-escluster.ps1
@@ -4,7 +4,7 @@ Param
     $rg = 'estemplate-poc-rg'
 )
 # start-escluster
-write-host "Starting all VMs in $rg"
+write-host "Starting all VMs in $rg..."
 get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg | Start-AzureRmVM
 # confirm final status
 get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg
--- src/stop-escluster.ps1
+++ src/stop-escluster.ps1
@@ -5,7 +5,7 @@ Param
     $rg = 'estemplate-poc-rg'
 )
 # stop-escluster
-write-host "Stopping all VMs in $rg"
+write-host "Stopping all VMs in $rg..."
 get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg | stop-AzureRmVM -Force
 # confirm final status
 get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg
