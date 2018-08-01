3d41505d61718518ab62353744e0b52e4213dca6
Darrell Lowrance <djl2@users.noreply.github.com>
2018-08-01 14:29:18 -0500

    Disable wait_for_started and apply_security_settings

--- src/scripts/elasticsearch-ubuntu-install.sh
+++ src/scripts/elasticsearch-ubuntu-install.sh
@@ -894,12 +894,12 @@ port_forward
 
 start_monit
 
-# 6.29.18 DISABLED for now due to timeout failure during VM creation script.  NEEDS RESEARCH
+# 8.01.18 DISABLED by Tethr DevOps due to timeout failure during VM creation script.
 # patch roles and users through the REST API which is a tad trickier
-if [[ ${INSTALL_XPACK} -ne 0 ]]; then
-wait_for_started
-apply_security_settings
-fi
+# if [[ ${INSTALL_XPACK} -ne 0 ]]; then
+# wait_for_started
+# apply_security_settings
+# fi
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 PRETTY=$(printf '%dh:%dm:%ds\n' $(($ELAPSED_TIME/3600)) $(($ELAPSED_TIME%3600/60)) $(($ELAPSED_TIME%60)))
