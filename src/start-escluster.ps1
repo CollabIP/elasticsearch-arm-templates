f7f06ba30e1370327f12419b3078bee148252296
Darrell Lowrance <djl2@users.noreply.github.com>
2018-08-20 15:30:02 -0500

    Update start cluster script for VM scale sets

--- src/start-escluster.ps1
+++ src/start-escluster.ps1
@@ -6,5 +6,8 @@ Param
 # start-escluster
 write-host "Starting all VMs in $rg..."
 get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg | Start-AzureRmVM
+write-host "Starting all VM Scale Sets in $rg..."
+get-azurermvmss | Where-Object -Property ResourceGroupName -eq $rg | Start-AzureRmVmss
 # confirm final status
-get-azurermvm -Status | Where-Object -Property ResourceGroupName -eq $rg
+get-azurermvm -status | Where-Object -Property ResourceGroupName -eq $rg | Format-Table
+get-azurermvmss | Where-Object -Property ResourceGroupName -eq $rg | Format-Table
