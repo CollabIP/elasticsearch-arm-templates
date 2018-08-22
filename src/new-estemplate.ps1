aad27ce1215ad2364083eca4e6114c755f915287
Darrell Lowrance <djl2@users.noreply.github.com>
2018-08-22 13:35:15 -0500

    Update to support more params

--- src/new-estemplate.ps1
+++ src/new-estemplate.ps1
@@ -26,8 +26,12 @@ For now, manually edit these parameters for the $clusterParameters array as need
 
 #>
 Param(
+    # Configure for correct base URL for github repo/branch'
+    [string]$sourceBaseUrl = 'https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/v6.3.1_feature-deploy-single-node-type/src',
     # Configure for vNet. If you want a new vNet created, enter 'new'; otherwise, enter 'existing'
     [string]$vNetNewOrExist = 'existing',
+    # Enter the Resource Group name
+    [string]$rg,
     # Enter node type. Options: master, data, or client. NOTE- clients deploys as a Scale Set.
     [string]$nodetype,
     # Enter a unique VM id number, e.g., 1,2,3,
@@ -45,11 +49,11 @@ Param(
 # $DebugPreference = "Continue"
 
 $clusterParameters = @{
-    "artifactsBaseUrl"="https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/v6.3.1_feature-deploy-single-node-type/src"
+    "artifactsBaseUrl"="$sourceBaseUrl"
     "esVersion" = "6.3.1"
     "esClusterName" = "elasticsearch"
     "vNetNewOrExisting" = "$vNetNewOrExist"
-    "vNetExistingResourceGroup" = "estemplate-poc-rg"
+    "vNetExistingResourceGroup" = "$rg"
     "xpackPlugins" = "Yes"
     "loadBalancerType" = "$LBtype"
     "nodeType" = "$nodetype"
@@ -76,8 +80,8 @@ $clusterParameters = @{
 # Capture all debug info in $output
 # Note that 5>&1 is a PS redirector operator. Required for capturing the debug output.
 $output = New-AzureRmResourceGroupDeployment `
-    -ResourceGroupName "estemplate-poc-rg" `
-    -TemplateUri "https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/v6.3.1_feature-deploy-single-node-type/src/mainTemplate.json" `
+    -ResourceGroupName "$rg" `
+    -TemplateUri "$sourceBaseUrl" `
     -TemplateParameterObject $clusterParameters `
     -DeploymentDebugLogLevel All
 
