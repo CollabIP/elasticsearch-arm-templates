b90f8a6a86989646b56492bfb73b8d073a0c6609
Darrell Lowrance <djl2@users.noreply.github.com>
2018-08-22 13:57:42 -0500

    Add param for resource group

--- src/new-estemplate.ps1
+++ src/new-estemplate.ps1
@@ -28,6 +28,8 @@ For now, manually edit these parameters for the $clusterParameters array as need
 Param(
     # Configure for vNet. If you want a new vNet created, enter 'new'; otherwise, enter 'existing'
     [string]$vNetNewOrExist = 'existing',
+    # Enter the Resource Group name
+    [string]$rg = "estemplate-poc-rg",
     # Enter node type. Options: master, data, or client. NOTE- clients deploys as a Scale Set.
     [string]$nodetype,
     # Enter a unique VM id number, e.g., 1,2,3,
@@ -49,7 +51,7 @@ $clusterParameters = @{
     "esVersion" = "6.3.1"
     "esClusterName" = "elasticsearch"
     "vNetNewOrExisting" = "$vNetNewOrExist"
-    "vNetExistingResourceGroup" = "estemplate-poc-rg"
+    "vNetExistingResourceGroup" = "$rg"
     "xpackPlugins" = "Yes"
     "loadBalancerType" = "$LBtype"
     "nodeType" = "$nodetype"
@@ -76,7 +78,7 @@ $clusterParameters = @{
 # Capture all debug info in $output
 # Note that 5>&1 is a PS redirector operator. Required for capturing the debug output.
 $output = New-AzureRmResourceGroupDeployment `
-    -ResourceGroupName "estemplate-poc-rg" `
+    -ResourceGroupName "$rg" `
     -TemplateUri "https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/v6.3.1_feature-deploy-single-node-type/src/mainTemplate.json" `
     -TemplateParameterObject $clusterParameters `
     -DeploymentDebugLogLevel All
