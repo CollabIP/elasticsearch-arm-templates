dd904272ebceadc76ab1f41545994f52a942034d
Darrell Lowrance <djl2@users.noreply.github.com>
2018-09-09 12:42:56 -0500

    Add "Tethr custom" to comments to clarify
    Non functional changes.

--- src/scripts/elasticsearch-ubuntu-install.sh
+++ src/scripts/elasticsearch-ubuntu-install.sh
@@ -994,22 +994,24 @@ configure_elasticsearch_yaml()
         echo "node.master: false" >> $ES_CONF
         echo "node.data: false" >> $ES_CONF
         echo "node.ingest: false" >> $ES_CONF
-        echo "# Enable CORS.  Needed for allowing elasticsearch-head to access this node" >> $ES_CONF
+        # Tethr custom. Add lines to support elasticsearch-head. Allows es-head to connect to es cluster
+        echo "# Tethr custom. Enable CORS.  Needed for allowing elasticsearch-head to access this node" >> $ES_CONF
         echo "http.cors.enabled: true" >> $ES_CONF
-        echo "# Set the origin to allow only the elasticsearch-head webapp URL to access" >> $ES_CONF
+        echo "# Tethr custom. Set the origin to allow only the elasticsearch-head webapp URL to access" >> $ES_CONF
         echo "http.cors.allow-origin: http://ctesdkibana:9100" >> $ES_CONF
-        echo "# Add header Authorization, in additional to the regular default values." >> $ES_CONF
+        echo "# Tethr custom. Add header Authorization, in additional to the regular default values." >> $ES_CONF
         echo "http.cors.allow-headers: Authorization, X-Requested-With, Content-Type, Content-Length" >> $ES_CONF
     elif [ ${INGEST_ONLY_NODE} -ne 0 ]; then
         log "[configure_elasticsearch_yaml] configure node as ingest only"
         echo "node.master: false" >> $ES_CONF
         echo "node.data: false" >> $ES_CONF
         echo "node.ingest: true" >> $ES_CONF
-        echo "# Enable CORS.  Needed for allowing elasticsearch-head to access this node" >> $ES_CONF
+        # Tethr custom. Add lines to support elasticsearch-head. Allows es-head to connect to es cluster
+        echo "# Tethr custom. Enable CORS.  Needed for allowing elasticsearch-head to access this node" >> $ES_CONF
         echo "http.cors.enabled: true" >> $ES_CONF
-        echo "# Set the origin to allow only the elasticsearch-head webapp URL to access" >> $ES_CONF
+        echo "# Tethr custom. Set the origin to allow only the elasticsearch-head webapp URL to access" >> $ES_CONF
         echo "http.cors.allow-origin: http://ctesdkibana:9100" >> $ES_CONF
-        echo "# Add header Authorization, in additional to the regular default values." >> $ES_CONF
+        echo "# Tethr custom. Add header Authorization, in additional to the regular default values." >> $ES_CONF
         echo "http.cors.allow-headers: Authorization, X-Requested-With, Content-Type, Content-Length" >> $ES_CONF
     else
         log "[configure_elasticsearch_yaml] configure node as master and data"
