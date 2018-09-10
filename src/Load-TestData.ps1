1d5202b4b656d701988841b5723ee917ecc8b6ff
Darrell Lowrance <djl2@users.noreply.github.com>
2018-09-10 12:38:42 -0500

    Fixed comment typos

--- src/Load-TestData.ps1
+++ src/Load-TestData.ps1
@@ -21,10 +21,10 @@ param(
                    Position=1)]
     $password
 )
-write-host 'Download shakesspeare_6.0.json data from ES website into user Download folder'
+write-host 'Download shakespeare_6.0.json data from ES website into user Download folder'
 invoke-webrequest https://download.elastic.co/demos/kibana/gettingstarted/shakespeare_6.0.json -outfile "~\downloads\shakespeare_6.0.json"
 
-write-host 'Load shakesspeare_6.0.json data into ES cluster'
+write-host 'Load shakespeare_6.0.json data into ES cluster'
 cd C:\Users\tethradmin\Downloads
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
 $logoutput = Invoke-RestMethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} "http://10.0.0.4:9200/shakespeare/doc/_bulk?pretty" -Method Post -ContentType 'application/x-ndjson' -InFile "~\Downloads\shakespeare_6.0.json"
