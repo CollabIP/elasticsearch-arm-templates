e4e9e25c0a336ca08f4f071eeb0ac579dce45045
Darrell Lowrance <darrell.lowrance@tethr.com>
2018-10-10 15:29:47 -0500

    Made several updates
    Added separate es user pw.. Disabled securityBootstrapPassword.  Should not be needed. Updated $rg to new name.

--- src/new-estemplate.ps1
+++ src/new-estemplate.ps1
@@ -50,33 +50,50 @@ For upgrades, define the new -esversion
 Param(
     # Enter the Github base URL
     [string]$sourceUrl = 'https://raw.githubusercontent.com/darrell-tethr/azure-marketplace/v6.3.1_feature-deploy-single-node-type/src',
+    
     # Enter the Elasticsearch version to be deployed. 
     [string]$esVersion = '6.4.0',
+    
     # Configure for new or existing vNet. An existing Virtual Network in another Resource Group in the same Location can be used.
     [string]$vNetNewOrExist = 'existing',
+    
     # Enter the vNetName. The Virtual Network must already exist when using an 'existing' Virtual Network
     [string]$vNetName = 'es-net',
+    
     # Enter the name of the Resource Group in which the Virtual Network resides when using an 'existing' Virtual Network. Required when using an 'existing' Virtual Network
-    [string]$rg = 'estemplate-poc-rg',
+    [string]$vNetExistingRG = 'estemplate-poc-rg',
+    
     # Enter the internal static IP address to use when configuring the internal load balancer
     [string]$vNetLoadBalancerIp = '10.0.0.4',
+    
     # The name of the subnet to which Elasticsearch nodes will be attached. The subnet must already exist when using an existing Virtual Network.
     [string]$vNetClusterSubnetName = 'es-subnet',
+    
     # Enter Ubuntu admin user
     [string]$ubuntuAdmin = 'russ',
+    
     # Enter Ubuntu admin password
     [string]$ubuntuPw = 'Password1234',
-    # Enter the password for Elasticsearch superuser 'elastic'
-    [string]$esPw = 'Password123',
+    
+    # Enter the password for built-in Elasticsearch superuser 'elastic'
+    [string]$elasticPw = 'Password123',
+    
+    # Enter the password for built-in Elasticsearch regular users
+    [string]$esUserPw = 'Password123',
+    
     # Enter node type. Options: master, data, or client
     [string]$nodetype,
+    
     # Enter a unique VM id number, e.g., 1,2,3. For Scale Sets, enter ss for ID purposes.
     [string]$vmid,
+    
     # Enter the Availability Zone number, e.g., 1, 2, or 3. Note: Scale Sets automatically deploy to all 3 zones.
     [string]$zone,
+    
     # Enter the Load Balancers type, i.e., internal or external.
     # Note: Only scale sets will be configured for Backend LB pool.
     [string]$LBtype ='external',
+    
     # Install Kibana if needed. IMPORTANT! Do not install with Master Node deploys. Options: Yes, No
     [string]$kibanainstall = "No"
 )
@@ -90,7 +107,7 @@ $clusterParameters = @{
     "esClusterName" = "elasticsearch"
     "vNetNewOrExisting" = "$vNetNewOrExist"
     "vNetName" = "$vNetName"
-    "vNetExistingResourceGroup" = "$rg"
+    "vNetExistingResourceGroup" = "$vNetExistingRG"
     "vNetLoadBalancerIp" = "$vNetLoadBalancerIp"
     "vNetClusterSubnetName" = "$vNetClusterSubnetName"
     "xpackPlugins" = "Yes"
@@ -100,6 +117,7 @@ $clusterParameters = @{
     "zoneId" = @("$zone")
     "kibana" = "$kibanainstall"
     "vmDataDiskCount" = 2
+    "vmDataDiskSize" = "Medium"   # Small (128Gb), Medium (512GB), Large (1024GB)
     "scaleSetInstanceCount" = "3"
     "vmHostNamePrefix" = "ctesd"
     "vmSizeMasterNodes" ="Standard_DS1_v2"
@@ -108,16 +126,16 @@ $clusterParameters = @{
     "vmSizeIngestNodes" = "Standard_DS1_v2"
     "adminUsername" = "$ubuntuAdmin"
     "adminPassword" = "$ubuntuPw"
-    "securityBootstrapPassword" = "$esPw"
-    "securityAdminPassword" = "$esPw"
-    "securityReadPassword" = "$esPw"
-    "securityKibanaPassword" = "$esPw"
-    "securityLogstashPassword" = "$esPw"
+    # "securityBootstrapPassword" = "$elasticPw"  DISABLED. Should be auto generated during deploy
+    "securityAdminPassword" = "$elasticPw"  # The password for built-in Elasticsearch superuser 'elastic'
+    "securityReadPassword" = "$esUserPw"
+    "securityKibanaPassword" = "$esUserPw"
+    "securityLogstashPassword" = "$esUserPw"
     }
 # Capture all debug info in $output
 # Note that 5>&1 is a PS redirector operator. Required for capturing the debug output.
 $output = New-AzureRmResourceGroupDeployment `
-    -ResourceGroupName "$rg" `
+    -ResourceGroupName "$vNetExistingRG" `
     -TemplateUri "$sourceUrl/mainTemplate.json" `
     -TemplateParameterObject $clusterParameters `
     -DeploymentDebugLogLevel All `
