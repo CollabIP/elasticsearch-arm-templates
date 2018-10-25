16527cd24fa2457aab912484b1defc84c8e03b84
Darrell Lowrance <darrell.lowrance@tethr.com>
2018-10-25 12:22:36 -0500

    Update default params for new RG

--- src/Encrypt-DataDisk.ps1
+++ src/Encrypt-DataDisk.ps1
@@ -6,19 +6,21 @@ Encrypt-DataDisk.ps1
  #>
 
 Param(
-    $rgName = 'estemplate-poc-rg', #VM�s Resource Group name
-    $vmName = 'ctesddata-3',  #VM name
-    $KeyVaultName = 'es-poc-kv', # KeyVault name
-    $keyEncryptionKeyName = 'ESpocKey', #Keyname. Must create in Azure in advance
+    $rgName = 'estemplate-poc-rg3', #VMs Resource Group name
+    $vmName = 'ctedeu2d01esd04',  #VM name
+    $KeyVaultName = 'es-dev-kv', # KeyVault name
+    $keyEncryptionKeyName = 'disk-key', #Keyname. Must create in Azure in advance
     $location = 'eastus2', #Keyvault region,
-    $volumeType = "OS"  # Select volume type to encrypt.  Options: All, OS, Data
+    $volumeType = "All"  # Select volume type to encrypt.  Options: All, OS, Data
 )
 $KeyVault = Get-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $rgName;
 $diskEncryptionKeyVaultUrl = $KeyVault.VaultUri;
 $KeyVaultResourceId = $KeyVault.ResourceId;
 $keyEncryptionKeyUrl = (Get-AzureKeyVaultKey -VaultName $KeyVaultName -Name $keyEncryptionKeyName).Key.kid;
 
-Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -KeyEncryptionKeyUrl $keyEncryptionKeyUrl -KeyEncryptionKeyVaultId $KeyVaultResourceId -SkipVmBackup -VolumeType $volumeType 
+Write-Host "Get start time" -ForegroundColor Green
+Get-Date
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -KeyEncryptionKeyUrl $keyEncryptionKeyUrl -KeyEncryptionKeyVaultId $KeyVaultResourceId -SkipVmBackup -VolumeType $volumeType -Verbose
 
 Write-Host 'Check disk status' -ForegroundColor Green
 Get-AzureRmVMDiskEncryptionStatus -ResourceGroupName $rgname -VMName $vmName
