7be19e1062d7802197d45b1e1afb3dd16d9a0762
Darrell Lowrance <darrell.lowrance@tethr.com>
2018-12-20 21:45:35 -0600

    Script is now functional.

--- src/Install-AgentToWindows.ps1
+++ src/Install-AgentToWindows.ps1
@@ -6,5 +6,17 @@ It's a global command that can be run on any Azure VM
 #>
 # Demo only.  For testing scripts
 #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://prod-api.agent.alienvault.cloud/osquery-api/us-west-2/bootstrap?flavor=powershell") | Invoke-Expression; install_agent -controlnodeid 91d63b61-4721-423e-91fe-cbda52cea0a0
+
+# Capture osqueryd service for if statement to check if it's alraedy installed
+$service = get-service -name osquery -ErrorAction SilentlyContinue
+
+# Install AlienVault
+# If $service is empty, the install script will run.
+if ($service ) {
+    Write-Host 'AlienVault Agent osqueryd is already installed'    
+} else {
+    Write-Host 'Installing AlienVault Agent'
+    #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+    #Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://prod-api.agent.alienvault.cloud/osquery-api/us-west-2/bootstrap?flavor=powershell") | Invoke-Expression; install_agent -controlnodeid 91d63b61-4721-423e-91fe-cbda52cea0a0
+}
